4c0282cbd1e8d9ec7bed8e9621c6c6e34da9b760
NeXFP <giovannycardoso50@gmail.com>
2021-10-29 19:47:12 -0400

    Started on adding functions and more questions

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const inquirer = require("inquirer");
-const {writeFile} = require("./utils/generateHTML");
+const fs = require('fs');
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const generateIndexHTML = require("./src/htmlTemplate");
 
+const teamArray = [];
 
 //Questions for Manager
 
 const managerQuestions = [
     {
         type:"input",
-        name:"managerName",
+        name:"name",
         message:"What is the manager's name?"
     },
     {
         type:"input",
-        name:"managerId",
+        name:"id",
         message:"What is the manager's ID?"
     },
     {
         type:"input",
-        name:"managerNumber",
+        name:"number",
         message:"What is the manager's office number?"
     },
     {
         type:"input",
-        name:"managerEmail",
+        name:"email",
         message:"Which email does the manager use?"
+    },
+    {
+        type:"input",
+        name:"github",
+        message:"What is the Github username that the manager would like to use?"
     },
     {
         type:"list",
@@ -33,3 +43,102 @@ const managerQuestions = [
         message: "Would you like to add an engineer?"
     }
 ];
+
+//Questions for Engineers
+
+const engineerQuestions = [
+    {
+        type:"input",
+        name:"name",
+        message:"What is the engineer's name?"
+    },
+    {
+        type:"input",
+        name:"id",
+        message:"What is the engineer's ID?"
+    },
+    {
+        type:"input",
+        name:"number",
+        message:"What is the engineer's office number?"
+    },
+    {
+        type:"input",
+        name:"email",
+        message:"Which email does the engineer use?"
+    },
+    {
+        type:"input",
+        name:"github",
+        message:"What is the Github username that the engineer would like to use?"
+    },
+    {
+        type:"list",
+        name:"internAdd",
+        choices:["Yes", "No"],
+        message: "Would you like to add an intern?"
+    }
+];
+
+//Questions for Interns
+
+const internQuestions = [
+    {
+        type:"input",
+        name:"name",
+        message:"What is the intern's name?"
+    },
+    {
+        type:"input",
+        name:"id",
+        message:"What is the intern's ID?"
+    },
+    {
+        type:"input",
+        name:"number",
+        message:"What is the interns's office number?"
+    },
+    {
+        type:"input",
+        name:"email",
+        message:"Which email does the intern use?"
+    },
+    {
+        type:"input",
+        name:"github",
+        message:"What is the Github username that the intern would like to use?"
+    }
+];
+//Prompts User if they'd like to add another employee to the team.
+const anotherOne = [
+    {
+        type: "list",
+        name: "nextMember",
+        message: "Select the type of team member you would like to add next, if you are done select 'Done' to generate your team. ",
+        choices: ['Engineer', 'Intern', 'Done']
+    }
+];
+
+//Function to prompt that each team will have a manager and begin with the manager function
+
+function init(){
+    managerPrompt();
+}
+
+
+//Function that will prompt the user to select the next employee type that they will be adding to their team
+
+function nextEmployee() {
+    inquirer.prompt(anotherOne).then((response) => {
+    console.log(response);
+    switch (response.nextMember) {
+        case "Engineer":
+            engineerPrompt();
+        case "Intern":
+            internPrompt();
+        case "Done":
+            console.log("Generating your team currently!")
+            makeTeam();
+    }
+    })
+}
